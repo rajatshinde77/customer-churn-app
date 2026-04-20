603a7ebecbc169865ee792159265fc86ee014dcc
Rajat Shinde <rajatshinde5005@gmail.com>
2026-04-20 15:21:14 +0530

    fixed mongo uri

--- mongo_db.py
+++ mongo_db.py
@@ -1,11 +1,14 @@
 from pymongo import MongoClient
 
 try:
-    MONGO_URI = "mongodb+srv://rajat:Rajat%40141099@cluster0.o14fppz.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://rajat:Rajat%40141099@cluster0.014fppz.mongodb.net/?retryWrites=true&w=majority"
+    
     client = MongoClient(MONGO_URI)
     db = client["churn_db"]
     predictions_collection = db["predictions"]
+
     print("MongoDB Connected Successfully")
+
 except Exception as e:
     print("MongoDB Connection Error:", e)
     predictions_collection = None
