15960dbb02923d3ae5b06cbdd0bba7c924f5ed1d
Rajat Shinde <rajatshinde5005@gmail.com>
2026-04-20 15:59:01 +0530

    fixed mongo final

--- mongo_db.py
+++ mongo_db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 try:
     # ✅ FINAL CORRECT CONNECTION STRING
-    MONGO_URI = "mongodb+srv://rajat:Rajat%40141099@cluster0.014fppz.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://rajat:<Rajat@141099>@cluster0.0i4fppz.mongodb.net/?appName=Cluster0"
 
     client = MongoClient(MONGO_URI)
 
