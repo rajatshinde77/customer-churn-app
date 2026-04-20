0ae799277921c910f313397692af588a1210bc52
Rajat Shinde <rajatshinde5005@gmail.com>
2026-04-20 14:43:54 +0530

    mongodb connected

--- mongo_db.py
+++ mongo_db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+MONGO_URI = "mongodb+srv://rajat:Rajat%40141099@cluster0.o14fppz.mongodb.net/?retryWrites=true&w=majority"
 
-db = client["churn_prediction_db"]
+client = MongoClient(MONGO_URI)
 
+db = client["churn_db"]
 predictions_collection = db["predictions"]
